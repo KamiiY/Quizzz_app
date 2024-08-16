4fb4974d1758a2c47633b08d271e23c30340a29f
KamiiY <camiipoliv@gmail.com>
2024-08-16 19:53:22 -0300

    Add hello world, commit test

--- Quizzz_app/Program.cs
+++ Quizzz_app/Program.cs
@@ -14,9 +14,10 @@ namespace Quizzz_app
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Console.WriteLine("Hello world");
         }
     }
 }
